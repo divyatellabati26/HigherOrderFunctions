685221245efb8eb4a22de17d955b8a67b0dbd878
divyatellabati26 <95356480+divyatellabati26@users.noreply.github.com>
2022-11-06 20:56:04 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import MyCart from "./MyCart";
+function App() {
+  return (
+    <div>
+      <MyCart />
+    </div>
+  );
+}
+export default App;
---
+++ src/data.js
@@ -0,0 +1,34 @@
+export const products = [
+  {
+    id: 1,
+    title: "shampoo",
+    price: 20,
+    quantity: 3,
+    discountpercent: 2,
+    discountprice: 15
+  },
+  {
+    id: 2,
+    title: "shampoo",
+    price: 20,
+    quantity: 2,
+    discountpercent: 2,
+    discountprice: 15
+  },
+  {
+    id: 3,
+    title: "shampoo",
+    price: 20,
+    quantity: 3,
+    discountpercent: 2,
+    discountprice: 15
+  },
+  {
+    id: 4,
+    title: "shampoo",
+    price: 20,
+    quantity: 2,
+    discountpercent: 2,
+    discountprice: 15
+  }
+];
